b7ec46df1a8bcac85c3f2f85c548e5c3cf1cedf0
Nikita Trofimov <ntr4@yandex.ru>
2022-06-01 12:28:32 +0500

    Fix after rebase

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Category, Genre, Title
 
 admin.site.register(Category)
 admin.site.register(Genre)
 admin.site.register(Title)
-=======
-
-# Register your models here.
->>>>>>> 1185c31 (Add core, reviews app)
