b13778dd5e957d4e2b77ffcbb632b5e41461f060
Александр Горыничев <px2005@yandex.ru>
2022-06-03 08:20:08 +0300

    sprint10-test-category-models-v1.01

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, filters
 from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import GenericViewSet
 
@@ -18,15 +18,15 @@ from users.models import UserProfile
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
-    lookup_field = 'slug'
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
-    lookup_field = 'slug'
 
 
 class TitleViewSet(viewsets.ModelViewSet):
