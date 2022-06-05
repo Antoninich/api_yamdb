a3cb71bb04dc28bac54724605283feeef60b82a8
Nikita Trofimov <ntr4@yandex.ru>
2022-06-05 17:58:38 +0500

    Delete docstring and comments

--- api_yamdb/core/views.py
+++ api_yamdb/core/views.py
@@ -2,9 +2,6 @@ from rest_framework import mixins, viewsets
 from rest_framework.response import Response
 
 class PatchModelMixin:
-    """
-    PATCH a model instance.
-    """
     def partial_update(self, request, *args, **kwargs):
         partial = True
         instance = self.get_object()
@@ -13,8 +10,6 @@ class PatchModelMixin:
         self.perform_update(serializer)
 
         if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
             instance._prefetched_objects_cache = {}
 
         return Response(serializer.data)
