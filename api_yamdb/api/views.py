bea8b15be77c03db7b719d88ebbe35e3ae2833eb
Александр Горыничев <px2005@yandex.ru>
2022-06-06 15:19:56 +0300

    sprint10-test-category-models-v1.11

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import get_object_or_404
-from rest_framework import mixins, filters, status, viewsets
+from rest_framework import filters, mixins, status, viewsets
 from rest_framework.response import Response
 
 from .filters import TitleFilter
