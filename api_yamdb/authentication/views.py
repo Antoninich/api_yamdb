993a4549a3a24d4390b878bfd0b1e3ee3bb1f56d
Antoninich <saa@it24.pw>
2022-06-06 10:54:07 +0300

    SlidingToken->AccessToken

--- api_yamdb/authentication/views.py
+++ api_yamdb/authentication/views.py
@@ -4,14 +4,14 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import SlidingToken
+from rest_framework_simplejwt.tokens import AccessToken
 
 from authentication.serializers import CreateUserSerializer, GetTokenSerializer
 from users.models import UserProfile
 
 
 def get_tokens_for_user(user):
-    token = SlidingToken.for_user(user)
+    token = AccessToken.for_user(user)
     return {
         'token': str(token),
     }
