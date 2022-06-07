044aaa88de547946f8de1087aaf9862223e0e2a8
Antoninich <saa@it24.pw>
2022-06-07 09:51:08 +0300

    restricting users in the admin panel

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -7,6 +7,7 @@ from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import UserProfile
 
 
+@admin.register(UserProfile)
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
@@ -45,6 +46,33 @@ class CustomUserAdmin(UserAdmin):
     search_fields = ('username', 'first_name', 'last_name', 'email')
     ordering = ('username',)
     filter_horizontal = ('groups', 'user_permissions',)
+    readonly_fields = [
+        'date_joined',
+    ]
 
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        disabled_fields = set()
+        if not is_superuser:
+            disabled_fields |= {
+                'username',
+                'is_superuser',
+                'user_permissions',
+            }
 
-admin.site.register(UserProfile, CustomUserAdmin)
+        if (
+            not is_superuser
+            and obj is not None
+            and obj == request.user
+        ):
+            disabled_fields |= {
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            }
+        for f in disabled_fields:
+            if f in form.base_fields:
+                form.base_fields[f].disabled = True
+        return form
