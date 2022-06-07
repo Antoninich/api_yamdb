a8e1cf6dc94924b8bbbb5376c01cf1ef7a92f3b1
Александр Горыничев <px2005@yandex.ru>
2022-06-07 17:33:26 +0300

    sprint10-test-category-models-v2.03

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('KEY', default=None)
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [os.getenv('HOSTS', default=None), ]
 
