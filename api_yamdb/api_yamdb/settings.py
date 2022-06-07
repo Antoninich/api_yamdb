eedba2b7bdd651f61dbb9c02ba7f5d5371fabe27
Antoninich <saa@it24.pw>
2022-06-07 09:51:48 +0300

    revert default permissions

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -109,7 +109,7 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
 }
 
