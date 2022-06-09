8c3b92549c9fdadc93d96b4e8cf340f21b143bab
Александр Горыничев <px2005@yandex.ru>
2022-06-09 15:22:23 +0300

    sprint10-test-category-models-v3.08

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -1,9 +1,4 @@
 from rest_framework import mixins, viewsets
-from rest_framework import filters
-from .permissions import (
-    IsAdmin,
-    ReadOnly,
-)
 
 
 class CreateListDestroyMixin(mixins.CreateModelMixin,
